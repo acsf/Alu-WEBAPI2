5822138065b344cb0b8e9af340a6bcd94e2c9c84
acsf <a.carlossf@hotmail.com>
2020-12-18 19:22:58 -0300

    Capitulo 2 - Fiddler

--- WEBAPI2/App_Start/WebApiConfig.cs
+++ WEBAPI2/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace WEBAPI2
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ WEBAPI2/Controllers/CarrinhoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Loja.DAO;
+using Loja.Models;
+
+namespace WEBAPI2.Controllers
+{
+    public class CarrinhoController : ApiController
+    {
+        public string Get(int id)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            Carrinho carrinho = dao.Busca(id);
+            return carrinho.ToXML();
+        }
+    }
+}
