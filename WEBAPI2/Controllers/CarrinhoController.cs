813e2530a834499110504efdc15edfd821ef7cb4
Antonio Filho <as185839@ncr.com>
2021-07-10 04:06:05 -0300

    Capitulo 6 - Status Code e Interface Uniforme

--- WEBAPI2/Controllers/CarrinhoController.cs
+++ WEBAPI2/Controllers/CarrinhoController.cs
@@ -12,18 +12,36 @@ namespace WEBAPI2.Controllers
     public class CarrinhoController : ApiController
     {
         //[HttpPost]
-        public Carrinho Get(int id)
+        public HttpResponseMessage Get(int id)
         {
-            CarrinhoDAO dao = new CarrinhoDAO();
-            Carrinho carrinho = dao.Busca(id);
-            return carrinho;
+            try
+            {
+                CarrinhoDAO dao = new CarrinhoDAO();
+                Carrinho carrinho = dao.Busca(id);
+                return Request.CreateResponse(HttpStatusCode.OK, carrinho);
+            }
+            catch (KeyNotFoundException)
+            {
+                string msg = string.Format("O carrinho {0} não foi encontrado", id);
+                HttpError error = new HttpError(msg);
+                return Request.CreateResponse(HttpStatusCode.NotFound, error);                
+            }
+           
         }
 
-        public string Post([FromBody]Carrinho carrinho)
+        public HttpResponseMessage Post([FromBody]Carrinho carrinho)
         {
             CarrinhoDAO dao = new CarrinhoDAO();
             dao.Adiciona(carrinho);
-            return "sucesso";
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+
+            //Uma boa prática passar a localização ao criar um objeto
+            //Para que outros possam localizar
+            string location = Url.Link("DefaultApi", new { controller = "carrinho", id = carrinho.Id });
+            response.Headers.Location = new Uri(location);
+            
+            return response;
         }
     }
 }
